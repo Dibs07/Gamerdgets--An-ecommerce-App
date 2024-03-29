453f75b411fb011d30de1ce0e06858b917612391
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-03-29 17:23:53 +0530

    modified:   Ecommerce/src/redux/cartSlice.jsx

--- Ecommerce/src/redux/cartSlice.jsx
+++ Ecommerce/src/redux/cartSlice.jsx
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = JSON.parse(localStorage.getItem('cart')) ?? [];
-//const initialState = [];
+//const initialState = JSON.parse(localStorage.getItem('cart')) ?? [];
+const initialState = [];
 export const cartSlice = createSlice({
     name: 'cart',
     initialState,
